fdd7e3bbaf8e8bef67ef8dc8458b4350eaf9d9a9
Vítězslav Dvořák <dvorak@austro-bohemia.cz>
2015-06-08 17:12:48 +0200

    Repackage

---
+++ libjs-fuelux.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+rm -rf build
+if [ -d "fuelux" ]; then
+  cd fuelux
+  git pull
+else
+  git clone https://github.com/ExactTarget/fuelux/
+  cd fuelux
+fi
+
+npm install
+npm run build
+npm run dist
+
+BUILD=`cat  ../build | perl -ne 'chomp; print join(".", splice(@{[split/\./,$_]}, 0, -1), map {++$_} pop @{[split/\./,$_]}), "\n";'`
+echo $BUILD > ../build
+
+VERSION=`cat package.json | grep '"version":' | awk -F'"' '{ print $4 }'`
+
+#CHANGES=`cat CHANGELOG.md | awk -vRS="##" 'NR<=2' ORS="##" | grep -v "##"`
+#dch -v $VERSION-$BUILD --package libjs-twitter-fuelux $CHANGES
+cd ..
+
+dch -v $VERSION-$BUILD --package libjs-fuelux 
+
+debuild -i -us -uc -b
+
+cd ..
+#~/bin/publish-deb-packages.sh
